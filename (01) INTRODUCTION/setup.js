ece5f276c48aaf08877c75abcb3c83b69d8ecdf9
VartikaSingh30 <vartikasingh3085@gmail.com>
2026-05-20 22:27:40 +0530

    changes

---
+++ (01) INTRODUCTION/setup.js
@@ -0,0 +1,12 @@
+// JavaScript Introduction
+
+console.log("Hello JavaScript");
+
+let studentName = "Vartika";
+let course = "JavaScript";
+let age = 21;
+
+console.log(studentName);
+console.log(course);
+console.log(age);
+
